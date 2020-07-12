845729d6b80a305da823851f24c0182ece560221
Noah <noah@propagandacreative.com>
2020-07-12 16:20:42 -0400

    Player walking updated

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ const source = require('vinyl-source-stream');
 const sourcemaps = require('gulp-sourcemaps');
 const buffer = require('vinyl-buffer');
 const tsify = require('tsify');
+const uglify = require('uglify-js');
+const watchify = require('watchify');
+
 
 
 function watch_ts(){
@@ -12,6 +15,20 @@ function watch_ts(){
     });
 }
 
+let b_instance = browserify({
+    debug: true,
+    cache: true,
+    entries: [require.resolve('babel-polyfill'), 'ts/main.ts'],
+    plugin: [watchify, tsify]
+});
+
+function compile_ts2() {
+    return b_instance.on('update', ev => {
+        return b_instance.bundle()
+            .pipe(source('index.js'))
+            .pipe(dest('js'));
+    });
+}
 
 function compile_ts() {
     try {
@@ -27,6 +44,7 @@ function compile_ts() {
                 extensions: ['.ts'],
 
             })
+            // .transform('uglifyify', { global: true })
             .bundle()
             .pipe(source('index.js'))
             .pipe(dest('js'));
@@ -37,3 +55,4 @@ function compile_ts() {
 
 exports.watch_ts = watch_ts;
 exports.compile_ts = compile_ts;
+exports.compile_ts2 = compile_ts2;
